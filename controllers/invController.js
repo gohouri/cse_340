8fab7e16fe5b5c690418cdde36a199818fe37e84
Ericevijay12 <ericevijayohiani@gmail.com>
2025-05-10 18:17:40 +0100

    Create invController.js

---
+++ controllers/invController.js
@@ -0,0 +1,17 @@
+const invController = {};
+
+// Build vehicles by classification view
+invController.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classificationId;
+  const data = await invModel.getInventoryByClassificationId(classification_id);
+  const grid = await utilities.buildClassificationGrid(data);
+  let nav = await utilities.getNav();
+  const className = data[0].classification_name;
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  });
+};
+
+module.exports = invController;
